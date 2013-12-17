df544c0bf5263fce7551f7213c14e013a09b382c
Michael Zheng <michaelzheng54@hotmail.com>
2013-12-17 16:30:24 +0000

    Almost done

--- welcomescript.js
+++ welcomescript.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
 	var greetingsArray = [ "Hello", "Hi There", "Howdy" ];
 	var item = greetingsArray[Math.floor(Math.random()*greetingsArray.length)];
     $("#header_greeting").html(item);
+    
+    $("a.clickable").bind('click', function() {
+    	navigateLink($(this).children("span").html());
+    });
 });
+
+function navigateLink(link) {
+	switch (link) {
+		case "Github": window.location.href = "http://www.github.com/michaelzheng"; break;
+		case "Twitter": window.location.href = "http://www.twitter.com/EXC_BAD_ACCESS"; break;
+		case "Linkedin": window.location.href = "http://www.linkedin.com/profile/view?id=156606696"; break;
+		case "Instagram": window.location.href = "http://www.instagram.com/EXC_BAD_ACCESS"; break;
+		case "Youtube": window.location.href = "http://www.youtube.com"; break;
+	}
+}
