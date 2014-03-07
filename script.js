3a621a651f19fb2939e5077a1f34eab4f867adf5
Michael Zheng <michaelzheng54@hotmail.com>
2014-03-07 22:38:20 +0000

    Revert "Try temp fix for google fonts"
    This reverts commit 29f3148fdfb93071a2bac1bd0d245d9494ed7a9e.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     $("a.clickable").bind('click', function() {
     	navigateLink($(this).children("span").html());
     });
-    $('body').hide().show();
 });
 
 function navigateLink(link) {
